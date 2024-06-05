a755570c26e2cd10deafd70a55814963f701960c
msalehprofile <mollyhannia@hotmail.co.uk>
2024-06-05 10:41:28 +0100

    got it connecting to sql

--- src/main/java/com/BookTrackerApplication/BookTracker/BookTrackerApplication.java
+++ src/main/java/com/BookTrackerApplication/BookTracker/BookTrackerApplication.java
@@ -2,9 +2,14 @@ package com.BookTrackerApplication.BookTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@EnableJpaRepositories("my.package.base.*")
+//@ComponentScan(basePackages = { "my.package.base.*" })
+//@EntityScan("my.package.base.*")
+@SpringBootApplication()
 public class BookTrackerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/BookTrackerApplication/BookTracker/BookTrackerService.java
+++ src/main/java/com/BookTrackerApplication/BookTracker/BookTrackerService.java
@@ -1,10 +1,12 @@
 package com.BookTrackerApplication.BookTracker;
 
+
 import com.BookTrackerApplication.BookTracker.Books.Books;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookTrackerService {
@@ -13,12 +15,22 @@ public class BookTrackerService {
 
     // create
     public void addBook(Books book) {
-        bookTrackerRepository.addBooks(book);
+        bookTrackerRepository.save(book);
     }
+
+
     // read
     public List<Books> getAllBooks() {
-        List<Books> storedBooks = bookTrackerRepository.getAllBooks();
-        return storedBooks;
+        return bookTrackerRepository.findAll();
+    }
+
+    public List<Books> getAllByAuthor(String authorName,  int limit) {
+        List<Books> books = bookTrackerRepository.getAllByAuthor(authorName);
+
+        return books
+                .stream()
+                .limit(limit)
+                .collect(Collectors.toList());
     }
 
 }
